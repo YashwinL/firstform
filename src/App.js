bdabdb7a4738d0ff0f454570d59ba567378f6e9b
YashwinL <yashwinlakshmipathy@gmail.com>
2021-11-12 20:34:45 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -22,13 +22,18 @@ function App() {
   const classes = useStyles();
   const [InputFields, setInputFields] = useState([
     { firstName: "", lastname: "" },
-    { firstName: "", lastname: "" },
   ]);
-  const handleChangeInput = (index, event) => {
-    console.log(index, event.target.name);
-    const values = [...InputFields];
-    values[index][event.target.name] = event.target.value;
-    setInputFields(values);
+  // const handleChangeInput = (index, event) => {
+  //   const { name, value } = event.target;
+  //   const values = [...InputFields];
+  //   values[index][name] = value;
+  //   setInputFields(values);
+  // };
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...InputFields];
+    list[index][name] = value;
+    setInputFields(list);
   };
 
   const handleSubmit = (e) => {
@@ -49,28 +54,41 @@ function App() {
     <Container>
       <h1>Add New Member</h1>
       <form className={classes.root} onSubmit={handleSubmit}>
-        {InputFields.map((InputFields, index) => (
-          <div key={InputFields.id}>
+        {InputFields.map((x, i) => (
+          <div className="box">
             <TextField
-              name="firstname"
-              label="First Name"
+              // name="firstname"
+              // label="First Name"
+              // variant="filled"
+              // value={InputFields.firstName}
+              // onChange={(event) => handleChangeInput(index, event)}
+              name="firstName"
+              placeholder="Enter First Name"
               variant="filled"
-              value={InputFields.firstName}
-              onChange={(event) => handleChangeInput(index, event)}
+              value={x.firstName}
+              onChange={(e) => handleInputChange(e, i)}
             />
             <TextField
-              name="lastname"
-              label="Last Name"
+              // name="lastname"
+              // label="Last Name"
+              // variant="filled"
+              // value={InputFields.lastname}
+              // onChange={(event) => handleChangeInput(index, event)}
+              className="ml10"
+              name="lastName"
+              placeholder="Enter Last Name"
               variant="filled"
-              value={InputFields.lastname}
-              onChange={(event) => handleChangeInput(index, event)}
+              value={x.lastName}
+              onChange={(e) => handleInputChange(e, i)}
             />
-            <IconButton onClick={() => handleRemoveFields(index)}>
-              <RemoveIcon />
-            </IconButton>
             <IconButton onClick={() => handleAddFields()}>
               <AddIcon />
             </IconButton>
+            {InputFields.length !== 1 && (
+              <IconButton onClick={() => handleRemoveFields(i)}>
+                <RemoveIcon />
+              </IconButton>
+            )}
           </div>
         ))}
         <Button
